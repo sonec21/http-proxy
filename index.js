cb28f80bb81dbd31a86d36fd099dc523746e0a31
Cristian Meza <cmeza@freedomforever.com>
2019-11-20 07:56:01 -0800

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+var httpProxy = require('http-proxy');
+
+httpProxy.createProxyServer({
+  target: 'https://stage.api.freedomforever.com',
+  https: true,
+  secure: false,
+  headers: {
+    host: 'api.freedomforever.com'
+  }
+}).listen(8012);
